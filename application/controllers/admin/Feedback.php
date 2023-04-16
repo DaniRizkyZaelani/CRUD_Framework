008dcf94dd1244b72daac32d0be095bc8525a967
unknown <danirizky704@gmail.com>
2023-04-17 04:12:22 +0700

    first commit

---
+++ application/controllers/admin/Feedback.php
@@ -0,0 +1,6 @@
+<?php 
+class Feedback extends CI_Controller{
+	public function index (){
+		$this->load->view('admin/feedback_list.php');
+	}
+}
---
+++ application/controllers/admin/Post.php
@@ -0,0 +1,6 @@
+<?php 
+class Post extends CI_Controller{
+	public function index (){
+		$this->load->view('admin/post_list.php');
+	}
+}
